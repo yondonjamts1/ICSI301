e5e287de7bac2aaa1a7836ea939eccd70fdec615
Yondonjamts <yondonjamts@outlook.com>
2018-02-15 14:09:57 +0800

    Added php files
    Added index and test php files for test Apache HTTP server environment.

---
+++ index.php
@@ -0,0 +1,7 @@
+<!--DOCTYPE html-->
+<html>
+<body>
+<p>A static HTML content</p>
+<?php echo "result of PHP script" ?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>
+		hello this is test
+	</title>
+</head>
+<body>
+	<p>
+		check test paragraph
+	</p>
+
+	<?php 
+$x = "Sain baina uu!";
+$y = 'Hello!';
+
+echo $x;
+echo "<br>";
+echo "<br>"; 
+echo $y;
+?>
+</body>
+</html>
